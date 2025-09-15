ca5287a00226eda23890fa89ed9fe3c69f766d29
Hans Halvorson <hhalvors@princeton.edu>
2025-09-15 06:18:17 -0400

    fine-tuned LaTeX proof output

--- src/ProofToLaTeX.hs
+++ src/ProofToLaTeX.hs
@@ -34,25 +34,32 @@ defaultRenderOpts = RenderOpts
 -- Public: make just the table
 proofTableLaTeX :: RenderOpts -> [ProofLine] -> String
 proofTableLaTeX opts ls =
-  let colSpec = ">{\\raggedleft\\arraybackslash}p{1.0cm}\
-                \ >{\\raggedright\\arraybackslash}p{1.6cm}\
-                \ p{9cm}\
+  let colSpec = ">{\\raggedleft\\arraybackslash}p{1.5cm}\
+                \ >{\\centering\\arraybackslash}p{1.0cm}\
+                \ p{5cm}\
                 \ >{\\raggedright\\arraybackslash}p{3.5cm}"
-      -- If you prefer exact widths, tweak the p{..} lengths above.
+
       rows     = map (renderLine opts) (sortOnLine ls)
-      hdr      = if header opts then headerRow (order opts) else ""
+
+      -- Make the header row, and ensure it ends with \\ \hline
+      hdrRow   = case header opts of
+                   True  -> headerRow (order opts) ++ " \\\\ \\hline"
+                   False -> ""   -- no header text, but we’ll still draw the top \hline
   in unlines $
        [ "\\begin{tabular}{" ++ colSpec ++ "}"
        , "\\hline"
-       , hdr
-       ] ++ intercalate ["\\\\ \\hline"] (map (:[]) rows)
-         ++ ["\\\\ \\hline", "\\end{tabular}"]
+       , hdrRow
+       ]
+       -- each row just ends with \\ (no \hline between or after)
+       ++ map (++ " \\\\") rows
+       ++ [ "\\end{tabular}" ]  
+
 
 -- Public: wrap table in a minimal, compilable LaTeX document
 proofDocumentLaTeX :: String -> RenderOpts -> [ProofLine] -> String
 proofDocumentLaTeX title opts ls =
   unlines
-    [ "\\documentclass[11pt]{article}"
+    [ "\\documentclass[12pt]{article}"
     , "\\usepackage[margin=1in]{geometry}"
     , "\\usepackage{amsmath,amssymb}"
     , "\\usepackage{array}"
@@ -71,7 +78,7 @@ proofDocumentLaTeX title opts ls =
 renderLine :: RenderOpts -> ProofLine -> String
 renderLine opts pl =
   let (ld, rd)   = mathDelims opts
-      lnTxt      = show (lineNumber pl)
+      lnTxt      = "(" ++ show (lineNumber pl) ++ ")"
       depsTxt    = ppRefs (references pl)
       formulaTxt = ld ++ ppFormulaLaTeX (formula pl) ++ rd
       justTxt    = ppJust (justification pl)
@@ -92,21 +99,47 @@ ppRefs s =
     []  -> "$\\varnothing$"           -- nice touch for empty set
     xs  -> intercalate "," (map show xs)
 
--- A compact, readable justification pretty-printer
+-- numbers-first, short-tag justification
 ppJust :: Justification -> String
-ppJust Assumption          = "Assumption"
-ppJust (MP m n)            = "MP "  ++ show m ++ " " ++ show n
-ppJust (MT m n)            = "MT "  ++ show m ++ " " ++ show n
-ppJust (DN m)              = "DN "  ++ show m
-ppJust (CP m n)            = "CP "  ++ show m ++ " " ++ show n
-ppJust (AndIntro m n)      = "$\\land$ Intro " ++ show m ++ " " ++ show n
-ppJust (AndElim m)         = "$\\land$ Elim "  ++ show m
-ppJust (OrIntro m)         = "$\\lor$ Intro "  ++ show m
-ppJust (OrElim d a p b c)  = "$\\lor$ Elim "   ++ unwords (map show [d,a,p,b,c])
-ppJust (ForallElim m)      = "$\\forall$ Elim "  ++ show m
-ppJust (ForallIntro m)     = "$\\forall$ Intro " ++ show m
-ppJust (ExistsIntro m)     = "$\\exists$ Intro " ++ show m
-ppJust (ExistsElim m a n)  = "$\\exists$ Elim "  ++ unwords (map show [m,a,n])
+ppJust Assumption = "A"
+ppJust j =
+  let tag :: String
+      tag = case j of
+        MP _ _               -> "MP"
+        MT _ _               -> "MT"
+        DN _                 -> "DN"
+        CP _ _               -> "CP"
+        AndIntro _ _         -> "∧I"
+        AndElim _            -> "∧E"
+        OrIntro _            -> "∨I"
+        OrElim _ _ _ _ _     -> "∨E"
+        RAA _ _              -> "RAA"
+        ForallElim _         -> "∀E"
+        ForallIntro _        -> "∀I"
+        ExistsIntro _        -> "∃I"
+        ExistsElim _ _ _     -> "∃E"
+        Assumption           -> "A"  -- already handled
+
+      nums :: [Int]
+      nums = case j of
+        MP m n               -> [m,n]
+        MT m n               -> [m,n]
+        DN m                 -> [m]
+        CP m n               -> [m,n]
+        AndIntro m n         -> [m,n]
+        AndElim m            -> [m]
+        OrIntro m            -> [m]
+        OrElim d a p b c     -> [d,a,p,b,c]
+        RAA a c              -> [a,c]
+        ForallElim m         -> [m]
+        ForallIntro m        -> [m]
+        ExistsIntro m        -> [m]
+        ExistsElim m a n     -> [m,a,n]
+        Assumption           -> []
+
+      numsTxt = intercalate "," (map show nums)
+  in if null nums then tag else numsTxt ++ " " ++ tag
+
 
 -- Sort by line number just in case the caller gives us unsorted lines
 sortOnLine :: [ProofLine] -> [ProofLine]
